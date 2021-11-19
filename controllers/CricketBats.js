be67a79faa7e9388cf6df6cddd26484006febdc5
sriharsha579 <skalluri579@gmail.com>
2021-11-19 12:35:28 -0600

    updated

--- controllers/CricketBats.js
+++ controllers/CricketBats.js
@@ -38,9 +38,17 @@ exports.CricketBats_create_post = async function(req, res) {
     }   
 }; 
  
-// Handle CricketBats delete form on DELETE. 
-exports.CricketBats_delete = function(req, res) { 
-    res.send('NOT IMPLEMENTED: CricketBats delete DELETE ' + req.params.id); 
+// Handle Costume delete on DELETE. 
+exports.CricketBats_delete = async function(req, res) { 
+    console.log("delete "  + req.params.id) 
+    try { 
+        result = await CricketBats.findByIdAndDelete( req.params.id) 
+        console.log("Removed " + result) 
+        res.send(result) 
+    } catch (err) { 
+        res.status(500) 
+        res.send(`{"error": Error deleting ${err}}`); 
+    } 
 }; 
  
 // Handle CricketBats update form on PUT. 
@@ -89,4 +97,60 @@ exports.CricketBats_detail = async function(req, res) {
     } 
 }; 
 
+ 
+ // Handle a show one view with id specified by query 
+ exports.CricketBats_view_one_Page = async function(req, res) { 
+    console.log("single view for id "  + req.query.id) 
+    try{ 
+        result = await CricketBats.findById( req.query.id) 
+        res.render('CricketBatsdetail',  
+{ title: 'CricketBats Detail', toShow: result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
 
+// Handle building the view for creating a costume. 
+// No body, no in path parameter, no query. 
+// Does not need to be async 
+exports.CricketBats_create_Page =  function(req, res) { 
+    console.log("create view") 
+    try{ 
+        res.render('CricketBatscreate', { title: 'CricketBats Create'}); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
+
+
+// Handle building the view for updating a costume. 
+// query provides the id 
+exports.CricketBats_update_Page =  async function(req, res) { 
+    console.log("update view for item "+req.query.id) 
+    try{ 
+        let result = await CricketBats.findById(req.query.id) 
+        res.render('CricketBatsupdate', { title: 'CricketBat Update', toShow: result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
+
+// Handle a delete one view with id from query 
+exports.CricketBats_delete_Page = async function(req, res) { 
+    console.log("Delete view for id "  + req.query.id) 
+    try{ 
+        result = await CricketBats.findById(req.query.id) 
+        res.render('CricketBatsdelete', { title: 'CricketBats Delete', toShow: 
+result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
--- routes/CricketBats.js
+++ routes/CricketBats.js
@@ -5,3 +5,18 @@ var router = express.Router();
 /* GET CricketBats */ 
 router.get('/', CricketBats_controlers.CricketBats_view_all_Page ); 
 module.exports = router;
+
+
+/* GET detail costume page */ 
+router.get('/detail', CricketBats_controlers.CricketBats_view_one_Page); 
+
+/* GET create costume page */ 
+router.get('/create', CricketBats_controlers.CricketBats_create_Page); 
+
+/* GET create update page */ 
+router.get('/update', CricketBats_controlers.CricketBats_update_Page);
+
+/* GET create costume page */ 
+router.get('/delete', CricketBats_controlers.CricketBats_delete_Page); 
+ 
+ 
