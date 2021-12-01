80d5c230d22bea11e9c0babcf2643517b1353b87
sriharsha579 <skalluri579@gmail.com>
2021-11-30 20:54:01 -0600

    final code committed

--- app.js
+++ app.js
@@ -28,22 +28,6 @@ var CricketBats = require("./models/CricketBats");
 var resourceRouter = require('./routes/resource'); 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/CricketBats', CricketBatsRouter);
-app.use('/addmods', addmodsRouter);
-app.use('/selector', selectorRouter);
-app.use('/resource', resourceRouter);
 
 passport.use(new LocalStrategy( 
   function(username, password, done) { 
@@ -75,6 +59,24 @@ var Account =require('./models/account');
 passport.use(new LocalStrategy(Account.authenticate())); 
 passport.serializeUser(Account.serializeUser()); 
 passport.deserializeUser(Account.deserializeUser());   
+
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/CricketBats', CricketBatsRouter);
+app.use('/addmods', addmodsRouter);
+app.use('/selector', selectorRouter);
+app.use('/resource', resourceRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/CricketBats.js
+++ routes/CricketBats.js
@@ -1,6 +1,16 @@
 var express = require('express'); 
 const CricketBats_controlers= require('../controllers/CricketBats'); 
-var router = express.Router(); 
+var router = express.Router();
+
+// A little function to check if we have an authorized user and continue on or 
+// redirect to login. 
+const secured = (req, res, next) => { 
+    if (req.user){ 
+      return next(); 
+    } 
+    req.session.returnTo = req.originalUrl; 
+    res.redirect("/login"); 
+  } 
  
 /* GET CricketBats */ 
 router.get('/', CricketBats_controlers.CricketBats_view_all_Page ); 
@@ -14,7 +24,7 @@ router.get('/detail', CricketBats_controlers.CricketBats_view_one_Page);
 router.get('/create', CricketBats_controlers.CricketBats_create_Page); 
 
 /* GET create update page */ 
-router.get('/update', CricketBats_controlers.CricketBats_update_Page);
+router.get('/update',secured,  CricketBats_controlers.CricketBats_update_Page);
 
 /* GET create costume page */ 
 router.get('/delete', CricketBats_controlers.CricketBats_delete_Page); 
